944d5f550ac2f22214a3d3b3e39063c5d6de3cf4
nada.amin <nada.amin@0b0c73a8-a84a-0410-a208-7d1b2017f9f7>
2008-04-24 00:51:37 +0000

    refactoring

--- coverage/coverage.lisp
+++ coverage/coverage.lisp
@@ -30,7 +30,7 @@
 	      (datum-lisp-form (tms-node-datum node)))
 	  (env-assumptions env)))
 
-(defun nutrients (env &aux forms nutrients)
+(defun env-nutrients (env &aux forms nutrients)
   (setq forms (env-forms env))
   (setq nutrients (remove-if-not #'(lambda (form)
 				     (eq (car form)
@@ -41,13 +41,18 @@
 		    (string-lessp (string a)
 				  (string b)))))
 
-(defun nutrients-sufficient-for-growth (&aux universal-node envs)
-  (setq universal-node (get-tms-node '(UNIVERSAL)))
+(defun organism-assumption (organism)
+  (if organism
+      `(organism ,organism)
+    '(UNIVERSAL)))
+
+(defun nutrients-sufficient-for-growth (&key (organism nil) &aux node envs)
+  (setq node (get-tms-node (organism-assumption organism)))
   (setq envs (assumptions-of '(GROWTH)))
   (setq envs (remove-if-not #'(lambda (env)
-			      (find universal-node (env-assumptions env)))
+			      (find node (env-assumptions env)))
 			    envs))
-  (mapcar #'nutrients envs))
+  (mapcar #'env-nutrients envs))
 
 (defun closed-environment-of (nutrients disabled-reactions &rest extra-forms &aux not-disabled-reactions)
   (setq nutrients (mapcar #'(lambda (nutrient) `(nutrient ,nutrient)) nutrients))
@@ -56,51 +61,45 @@
 					 (fetch '(not (disabled-reaction ?r)))))
   (environment-of (append extra-forms nutrients not-disabled-reactions)))
 
-(defun direct-outcome (nutrients disabled-reactions &aux env)
-  (setq env (closed-environment-of nutrients disabled-reactions '(UNIVERSAL)))
+(defun direct-outcome (nutrients disabled-reactions &key (organism nil) &aux env)
+  (setq env (closed-environment-of nutrients disabled-reactions (organism-assumption organism)))
   (if (in? '(GROWTH) env) 'GROWTH 'NO-GROWTH))
 
-(defun recs-of-env (recs)
-  #'(lambda (le) 
+(defun forms->env->env-forms (forms)
+  #'(lambda (env) 
       (mapcar 
        #'cadr
        (remove-if-not 
-	#'(lambda (rec) 
-	    (in-node? (get-tms-node rec) le))
-	recs))))
+	#'(lambda (form) 
+	    (in-node? (get-tms-node form) env))
+	forms))))
 
-(defun growth->no-growth (nutrients disabled-reactions extra-forms &aux env les recs recs-of-env)
-  (setq env (closed-environment-of nutrients disabled-reactions '(UNIVERSAL)))
+(defun unique-env-sets (les forms)
+  (remove-duplicates (mapcar (forms->env->env-forms forms) les) :test #'equal))
+
+(defun growth->no-growth (nutrients disabled-reactions &rest extra-forms &aux env les recs recs-of-env)
+  (setq env (apply #'closed-environment-of `(,nutrients ,disabled-reactions ,@extra-forms)))
   (setq les (remove-if-not #'(lambda (le) (subset-env? le env))
 			   (tms-node-label (get-tms-node '(GROWTH)))))
   (setq recs (fetch '(enabled-reaction ?r)))
-  (remove-duplicates (mapcar (recs-of-env recs) les) :test #'equal))
+  (unique-env-sets les recs))
 
-(defun just-nutrients (env)
-  (mapcar #'cadr
-   (remove-if-not
-    #'(lambda (form)
-	(eq (car form) 'nutrient))
-    (mapcar #'(lambda (node)
-	       (datum-lisp-form (tms-node-datum node))) 
-	    (env-assumptions env)))))
-
-(defun no-growth->growth (nutrients disabled-reactions extra-forms &aux env les)
+(defun no-growth->growth (nutrients disabled-reactions &rest extra-forms &aux env les)
   (setq env (environment-of extra-forms))
   (unless (consistent-with? '(GROWTH) env)
     (return-from no-growth->growth nil))
   (setq les (remove-if-not #'(lambda (le)
 			       (and (subset-env? env le)
-				    (subsetp (just-nutrients le) nutrients)))
+				    (subsetp (env-nutrients le) nutrients)))
 			   (tms-node-label (get-tms-node '(GROWTH)))))
   (setq recs (mapcar #'(lambda (rec) `(enabled-reaction ,rec)) disabled-reactions))
-  (remove-duplicates (mapcar (recs-of-env recs) les) :test #'equal))
+  (unique-env-sets les recs))
 
-(defun flip-outcome (nutrients disabled-reactions &aux outcome)
-  (setq outcome (direct-outcome nutrients disabled-reactions))
+(defun flip-outcome (nutrients disabled-reactions &key (organism nil) &aux outcome)
+  (setq outcome (direct-outcome nutrients disabled-reactions :organism organism))
   (cond ((eq outcome 'growth)
 	 (format t "Outcome is growth.~% For no-growth, disable ONE reaction from EACH set:")
-	 (growth->no-growth nutrients disabled-reactions '((UNIVERSAL))))
+	 (growth->no-growth nutrients disabled-reactions (organism-assumption organism)))
 	(t
 	 (format t "Outcome is no-growth.~% For growth, enable EACH reaction from ONE set:")
-	 (no-growth->growth nutrients disabled-reactions '((UNIVERSAL))))))
+	 (no-growth->growth nutrients disabled-reactions (organism-assumption organism)))))
