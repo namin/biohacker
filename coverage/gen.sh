fd38dbb854e956bda375ae7d96715a31ca974ee1
nada.amin <nada.amin@0b0c73a8-a84a-0410-a208-7d1b2017f9f7>
2008-05-03 07:53:35 +0000

    switched to jtms from atms

--- coverage/gen.sh
+++ coverage/gen.sh
@@ -8,8 +8,12 @@ cd ..
 cd coverage
 
 echo "; Load the BPS-related code needed by all coverage files" >init.lisp
+echo "; Load the BPS-related code needed by all jcoverage files" >jinit.lisp
 echo "(load (concatenate 'string \"$bps\" \"utils/init.lisp\"))" >>init.lisp
+echo "(load (concatenate 'string \"$bps\" \"utils/init.lisp\"))" >>jinit.lisp
 echo "(load (concatenate 'string \"$bps\" \"atms/atre.lisp\"))" >>init.lisp
+echo "(load (concatenate 'string \"$bps\" \"jtms/jtre.lisp\"))" >>jinit.lisp
 echo "(compile-atre)" >>init.lisp
+echo "(compile-jtre)" >>jinit.lisp
 
 
