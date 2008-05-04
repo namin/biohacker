7c6795813c00f3fefbeca7256ddb9d376fbc40fe
nada.amin <nada.amin@0b0c73a8-a84a-0410-a208-7d1b2017f9f7>
2008-05-04 11:04:39 +0000

    tried to do ex4 in chapter10, though not sure if solution is a hack...

--- trunk/BPS/ltms/indirect.lisp
+++ trunk/BPS/ltms/indirect.lisp
@@ -35,6 +35,66 @@
 		 (run-rules)))
    (known? fact)))
 
+;; Ex 4 in Chapter 10
+
+(defun assumptions-and-clauses-of-clause (in-clause &aux)
+  (do ((clause-queue (list in-clause)
+		     (nconc (cdr clause-queue) new-clauses))
+       (mark (list nil))
+       (node nil)
+       (new-clauses nil nil)
+       (assumptions nil)
+       (clauses (list in-clause)))
+      ((null clause-queue) (values assumptions clauses))
+    (dolist (term-pair (clause-literals (car clause-queue)))
+      (setq node (car term-pair))
+      (unless (eq (tms-node-mark node) mark)
+	(unless (eq (tms-node-label node) (cdr term-pair))
+	  (cond ((eq :ENABLED-ASSUMPTION (tms-node-support node))
+		 (push node assumptions))
+		((null (tms-node-support node)) (ltms-error "Node is unknown" node))
+		(t 
+		 (push (tms-node-support node) clauses)
+		 (push (tms-node-support node) new-clauses))))
+	(setf (tms-node-mark node) mark)))))
+
+(defun clause->formula (clause)
+  (cons ':OR (mapcar #'(lambda (x)
+			 (if (eq (cdr x) :FALSE) `(:NOT ,(car x))
+			   (car x)))
+		     (clause-literals clause))))
+
+(defun try-indirect-proof-better (fact &optional (*LTRE* *LTRE*))
+  (unless (known? fact)
+    (with-contradiction-handler (ltre-ltms *ltre*)
+      #'(lambda (contradictions ltms &aux contradiction ante-formulas formula)
+	  (setq contradiction (car contradictions))
+	  (multiple-value-bind 
+	      (assumptions clauses) 
+	      (assumptions-and-clauses-of-clause contradiction)
+	    (let ((the-node
+		   (find (datum-tms-node (referent fact T))
+			 assumptions)))
+	      (when the-node
+		(let ((status (tms-node-label the-node)))
+		  (retract-assumption the-node)
+		  (setq ante-formulas 
+			(mapcar #'clause->formula
+				(remove-duplicates clauses)))
+		  (setq formula 
+			`(:IMPLIES (:AND ,@ante-formulas)
+				   ,(ecase status
+				      (:TRUE `(:NOT ,the-node))
+				      (:FALSE the-node))))
+		(add-nogood the-node status
+			    assumptions
+			    (list :IMPLIED-BY formula 'INDIRECT-PROOF))
+)))))
+      ;; Assume the negation
+       (assuming `((:NOT ,fact)) *LTRE*
+		 (run-rules)))
+   (known? fact)))
+
 ;;;; Example of indirect proof
 
 (defun indirect-proof-example ()
@@ -43,5 +103,5 @@
   (assert! '(:IMPLIES p r) 'user)
   (assert! '(:IMPLIES q r) 'user)
   (known? 'r)
-  (try-indirect-proof 'r)
+  (try-indirect-proof-better 'r)
   (known? 'r))
