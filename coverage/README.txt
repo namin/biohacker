172696ccf96c093f0713538f8b4689436441e76e
nada.amin <nada.amin@0b0c73a8-a84a-0410-a208-7d1b2017f9f7>
2008-05-04 05:09:27 +0000

    added LTMS system

--- coverage/README.txt
+++ coverage/README.txt
@@ -1,5 +1,10 @@
-Just once, run ./gen.sh to generate init.lisp and jinit.lisp.
-Then, to get started, EITHER
-- load init.lisp and one of coverage.lisp, coverage2.lisp, check-growth.lisp
-- load jinit.lisp and jcoverage.lisp
-You can then load a specific experiment file, like organism-ex.lisp.
+Just once, run ./gen.sh to generate init.lisp, jinit.lisp and linit.lisp.
+
+Then, to get started, choose ONE of:
+- ATMS: load init.lisp and one of coverage.lisp, coverage2.lisp, check-growth.lisp
+- JTMS: load jinit.lisp and jcoverage.lisp
+- LTMS: load linit.lisp and lcoverage.lisp
+
+You can then load a specific experiment file, like organism-ex.lisp or
+real-ex.lisp, which comes with real-not-produced-j.lisp (for JTMS) and
+real-not-produced-l.lisp (for LTMS) to be loaded afterwards.
--- coverage/gen.sh
+++ coverage/gen.sh
@@ -9,11 +9,16 @@ cd coverage
 
 echo "; Load the BPS-related code needed by all coverage files" >init.lisp
 echo "; Load the BPS-related code needed by all jcoverage files" >jinit.lisp
+echo "; Load the BPS-related code needed by all lcoverage files" >linit.lisp
 echo "(load (concatenate 'string \"$bps\" \"utils/init.lisp\"))" >>init.lisp
 echo "(load (concatenate 'string \"$bps\" \"utils/init.lisp\"))" >>jinit.lisp
+echo "(load (concatenate 'string \"$bps\" \"utils/init.lisp\"))" >>linit.lisp
 echo "(load (concatenate 'string \"$bps\" \"atms/atre.lisp\"))" >>init.lisp
 echo "(load (concatenate 'string \"$bps\" \"jtms/jtre.lisp\"))" >>jinit.lisp
+echo "(load (concatenate 'string \"$bps\" \"ltms/ltre.lisp\"))" >>linit.lisp
 echo "(compile-atre)" >>init.lisp
 echo "(compile-jtre)" >>jinit.lisp
+echo "(load-ltre)" >>linit.lisp
+
 
 
