c6226806dc865df41e78f6d6b874f5b4aa7ab483
nada.amin <nada.amin@0b0c73a8-a84a-0410-a208-7d1b2017f9f7>
2008-05-04 05:09:27 +0000

    added LTMS system

---
+++ trunk/coverage/lcoverage.lisp
@@ -0,0 +1,99 @@
+(defvar *coverage-path*
+  (make-path *trunk-home* "coverage"))
+
+(defvar *coverage-rules-file*
+  (make-bps-source-file-name *coverage-path* "lcoverage-rules"))
+
+(defvar *debug-rules-file*
+  (make-bps-source-file-name *coverage-path* "lcoverage-debug-rules"))
+
+(defvar *debugging-coverage* t)
+
+(defvar *organism* nil)
+
+(defmacro debugging-coverage (msg &rest args)
+  `(when *debugging-coverage* (format t ,msg ,@ args)))
+
+(defun create-coverage-problem (&key (debugging nil))
+  (setq *jtre* (create-ltre "Coverage Problem" :debugging debugging))
+  (setq *organism* nil)
+  (load *coverage-rules-file*)
+  *jtre*)
+
+(defmacro reaction (name reactants &rest products)
+  `(assert! '(:IMPLIES (reaction-enabled ,name) (reaction ,name ,reactants ,@products)) ':REACTION))
+
+(defmacro growth (&rest compounds)
+  `(assert! '(:IMPLIES (:AND ,@ (mapcar #'(lambda (compound)
+					    `(compound ,compound))
+					compounds)) 
+		       growth)
+	    ':SUFFICIENT-FOR-GROWTH))
+
+(defmacro organism (name))
+
+(defmacro enzyme (enzyme &rest genes &aux gene-forms statements)
+  (setq gene-forms
+	(mapcar #'(lambda (gene)
+		   (let ((gene-form `(gene ,gene))) 
+		     (push `(assert! '(:IMPLIES (gene-on ,gene) ,gene-form) ':GENE) statements)
+		     gene-form))
+		genes))
+  (push `(assert! '(:IMPLIES (:AND ,@gene-forms) (enzyme ,enzyme))
+		  ':ENZYME)
+	statements)
+  (push 'progn statements)
+  statements)
+
+(defmacro catalyze (reaction &rest enzymes &aux enzyme-forms)
+  (setq enzyme-forms (mapcar #'(lambda (enzyme) `(enzyme ,enzyme)) enzymes))
+  `(assert! '(:IMPLIES (:AND ,@enzyme-forms) (reaction-enabled ,reaction)) ':CATALYZE))
+
+(defun retract-all-experiments ()
+  (dolist (form (fetch '(experiment . ?x)))
+    (when (known-node? (referent form))
+      (retract! form ':EXPERIMENT))))
+
+(defmacro experiment (outcome &key (nutrients nil) (off nil))
+  `(progn
+     (retract-all-experiments)
+     (let ((exp-form '(experiment ,outcome ,nutrients ,@off)))
+       (assume! exp-form ':EXPERIMENT)
+       (run-rules)
+       (true? 'growth))))
+
+(defun load-debug-rules ()
+  (load *debug-rules-file*)
+  (run-rules))
+
+(defun reactions-for-product (compound)
+  (mapcar #'(lambda (form) (caddr form)) 
+	  (fetch `(product ,compound ?r))))
+
+(defun reactants-for-reaction (reaction)
+  (mapcar #'(lambda (form) (cadr form)) 
+	  (fetch `(reactant ?r ,reaction))))
+
+(defun sort-symbols (symbols)
+  (sort symbols 
+	#'(lambda (x y) (string-lessp (string x) (string y)))))
+
+(defun why-not? (compound &optional &aux or-and-list)
+  (if (true? `(compound ,compound))
+      (progn
+	(format t "~%Compound ~A is produced." compound)
+	t)
+    (progn
+      (dolist (reaction (reactions-for-product compound))
+      (let ((missing-reactants 
+	     (sort-symbols
+	      (remove-if 
+	       #'(lambda (reactant) (true? `(compound ,reactant))) 
+	       (reactants-for-reaction reaction)))))
+	(format t "~%Reaction ~A missing reactants ~A" 
+		reaction 
+		missing-reactants)
+	(push (cons reaction missing-reactants) or-and-list)))
+    (unless or-and-list
+      (format t "~%Compound ~A is not a product." compound))
+    or-and-list)))
