cef3ec0915b0515778594be721b6e42a8f29214a
nada.amin <nada.amin@0b0c73a8-a84a-0410-a208-7d1b2017f9f7>
2008-05-05 07:09:42 +0000

    added, as comments, output of example queries

--- trunk/BPS/ltms/abduction-ex.lisp
+++ trunk/BPS/ltms/abduction-ex.lisp
@@ -27,7 +27,15 @@
 (setq patterns '((son ?son of ?father) (father ?father of ?son) (brothers ?a ?b)))
 
 (needs '(father mohamed of aadel) :TRUE patterns)
+; (((SON AADEL OF MOHAMED)))
+
 (needs '(related mohamed aadel) :TRUE patterns)
+;(((SON AADEL OF MOHAMED)) 
+; ((FATHER MOHAMED OF AADEL))
+; ((BROTHERS AADEL MOHAMED)) 
+; ((BROTHERS MOHAMED AADEL))
+; ((FATHER AADEL OF MOHAMED)) 
+; ((SON MOHAMED OF AADEL)))
 
 (setq pattern-cost-list
       '(((son ?son of ?father) . 20) 
@@ -35,3 +43,4 @@
 	((brothers ?a ?b) . 30)))
 
 (labduce '(related mohamed aadel) :TRUE pattern-cost-list)
+; (((FATHER MOHAMED OF AADEL)) . 10)
