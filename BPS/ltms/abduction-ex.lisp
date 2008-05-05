a812c4589c9be38c24fb8caf379db444e4551610
nada.amin <nada.amin@0b0c73a8-a84a-0410-a208-7d1b2017f9f7>
2008-05-05 06:31:01 +0000

    added example; cleaned up code

---
+++ BPS/ltms/abduction-ex.lisp
@@ -0,0 +1,26 @@
+(in-ltre (create-ltre "Abduction Test"))
+
+(rule ((:INTERN (son ?son of ?father) :var ?def))
+      (rassert! (:IMPLIES ?def (father ?father of ?son))
+		:EQUIVALENCE)
+      (rassert! (:IMPLIES ?def (related ?son ?father)) :FAMILY)
+      (rassert! (:IMPLIES ?def (related ?father ?son))) :FAMILY)
+
+(rule ((:INTERN (father ?father of ?son) :var ?def))
+      (rassert! (:IMPLIES ?def (son ?son of ?father))
+		:EQUIVALENCE)
+      (rassert! (:IMPLIES ?def (related ?son ?father)) :FAMILY)
+      (rassert! (:IMPLIES ?def (related ?father ?son))) :FAMILY)
+
+(rule ((:INTERN (brothers ?a ?b) :var ?def))
+      (rassert! (:IMPLIES ?def (brothers ?b ?a)) :FAMILY)
+      (rassert! (:IMPLIES ?def (related ?a ?b)) :FAMILY)
+      (rassert! (:IMPLIES ?def (related ?b ?a)) :FAMILY))
+
+;(rule ((:INTERN (related ?a ?b) :var ?def))
+;(rassert! (:IMPLIES ?def
+;(:OR (brothers ?a ?b)
+;(father ?a of ?b)
+;(son ?a of ?b)))))
+
+(needs '(father mohamed of aadel) :TRUE)
