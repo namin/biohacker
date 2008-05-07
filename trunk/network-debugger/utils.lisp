bae78a9d569d7b879fae646e4e6c8791faaa7682
nada.amin <nada.amin@0b0c73a8-a84a-0410-a208-7d1b2017f9f7>
2008-05-07 22:22:26 +0000

    minor reorg

--- trunk/network-debugger/utils.lisp
+++ trunk/network-debugger/utils.lisp
@@ -1,3 +1,9 @@
+(defun list-of (name els)
+  (mapcar
+   #'(lambda (el)
+     (list name el))
+   els))
+
 (defun pp-facts (facts &optional (st t))
   (dolist (fact facts)
     (format st "~A~%" (view-fact fact))))
