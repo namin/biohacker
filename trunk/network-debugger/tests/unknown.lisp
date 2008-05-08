c2c076eface3c6ebea36f1a33907b49e2b113879
nada.amin <nada.amin@0b0c73a8-a84a-0410-a208-7d1b2017f9f7>
2008-05-08 04:27:46 +0000

    unknown and unknown-enzyme tests working

--- trunk/network-debugger/tests/unknown.lisp
+++ trunk/network-debugger/tests/unknown.lisp
@@ -39,3 +39,38 @@
  :growth? t
  :essential-compounds (a e)
  :knock-outs (g1))
+
+(experiment
+ no-growth-due-to-unknown
+ (a b)
+ :growth? nil
+ :essential-compounds (a e))
+
+(experiment
+ no-growth-independent-of-unknowns
+ (a)
+ :growth? nil
+ :essential-compounds (a e))
+
+#|
+Network Debugger UNKNOWN
+Adding reaction R1.
+Adding reaction R2.
+Adding reaction R3.
+Adding reaction R4.
+Adding enzyme E1.
+Adding enzyme E2.
+Adding enzyme E3.
+Adding enzyme E4.
+Closing network for EXPERIMENT.
+Adding experiment GROWTH-DUE-TO-UNKNOWN
+Assuming simplify-investigations.
+Assuming unknown genes and reaction reversibilities as convenient.
+Focusing on experiment GROWTH-DUE-TO-UNKNOWN. Experiment GROWTH-DUE-TO-UNKNOWN is coherent.
+Adding experiment NO-GROWTH-DUE-TO-UNKNOWN
+Retracting focus on experiment GROWTH-DUE-TO-UNKNOWN.
+Focusing on experiment NO-GROWTH-DUE-TO-UNKNOWN. Experiment NO-GROWTH-DUE-TO-UNKNOWN is coherent.
+Adding experiment NO-GROWTH-INDEPENDENT-OF-UNKNOWNS
+Retracting focus on experiment NO-GROWTH-DUE-TO-UNKNOWN.
+Focusing on experiment NO-GROWTH-INDEPENDENT-OF-UNKNOWNS. Experiment NO-GROWTH-INDEPENDENT-OF-UNKNOWNS is coherent.
+|#
