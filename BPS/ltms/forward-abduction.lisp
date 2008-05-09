cec68caee5fa6ee0753483f02279420cb9168b82
nada.amin <nada.amin@0b0c73a8-a84a-0410-a208-7d1b2017f9f7>
2008-05-09 20:21:31 +0000

    forget file

---
+++ BPS/ltms/forward-abduction.lisp
@@ -0,0 +1,20 @@
+(defun keep-assuming (fact label patterns &key (debugging t) &aux all-assumed)
+  (dolist (pattern patterns)
+    (dolist (new-fact (fetch pattern))
+      (unless (known? new-fact)
+	(when debugging
+	  (format t "~%Assuming ~A for forward abduction" new-fact))
+	(assume! new-fact :FORWARD-ABDUCTION)
+	(push new-fact all-assumed)
+	(when (known? fact) ;; should check label as sanity check
+	  (return-from keep-assuming all-assumed))))))
+
+(defun needs-forward (fact label patterns &key (debugging t) &aux all-assumed antecedents)
+  (setq all-assumed (keep-assuming fact label patterns :debugging debugging))
+  (setq antecedents 
+	(remove-if-not #'already-assumed? (all-antecedents (signed-view-node (get-tms-node fact)) patterns)))
+  (dolist (assumed-fact all-assumed)
+    (when debugging
+      (format t "~%Retracting ~A after forward abduction" assumed-fact))
+    (retract! assumed-fact :FORWARD-ABDUCTION))
+  antecedents)
