efcb1b8af094ea03d08325344081f4de730f24bc
nada.amin <nada.amin@0b0c73a8-a84a-0410-a208-7d1b2017f9f7>
2008-05-09 23:03:21 +0000

    lessons

--- network-debugger/utils.lisp
+++ network-debugger/utils.lisp
@@ -8,3 +8,12 @@
   (dolist (fact facts)
     (format st "~A~%" (view-fact fact))))
 
+(defmacro tofile (filename &body body)
+  `(with-open-file (file ,filename
+		    :direction :output
+		    :if-exists :supersede)
+     (let ((*standard-output* file))
+       ,@body) ))
+
+(defun pathway-tools-file (filename lst)
+(tofile filename (loop for el in lst do (format t "~A	10~%" el))))
