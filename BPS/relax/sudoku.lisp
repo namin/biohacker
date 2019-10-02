687e0f0877016c6d2c0be965393e1e0f328d29dc
Nada Amin <namin@alum.mit.edu>
2019-10-02 00:04:27 -0400

    cosmetics

--- BPS/relax/sudoku.lisp
+++ BPS/relax/sudoku.lisp
@@ -18,54 +18,55 @@
 
 (defun create-puzzle (puzzle)
   (let ((net (create-network "sudoku")))
-    (loop
-        for i from 1 to 9
-        do (loop
-               for j from 1 to 9
-               do (build-cell (name i j) net
-                              (let ((x (elt (elt puzzle (- i 1)) (- j 1))))
-                                (if (= 0 x)
-                                    (loop for v from 1 to 9 collect v)
-                                  (list x))))))
-    net))
+  (loop for i from 1 to 9 do
+  (loop for j from 1 to 9 do
+  (build-cell
+   (name i j) net
+   (let ((x (elt (elt puzzle (- i 1)) (- j 1))))
+     (if (= 0 x)
+         (loop for v from 1 to 9 collect v)
+       (list x))))))
+  net))
 
 (defun rows ()
-  (loop
-      for i from 1 to 9
-      collect (loop for j from 1 to 9
-                  collect (name i j))))
+  (loop for i from 1 to 9 collect
+  (loop for j from 1 to 9 collect
+  (name i j))))
 
 (defun cols ()
-  (loop
-      for i from 1 to 9
-      collect (loop for j from 1 to 9
-                  collect (name j i))))
+  (loop for i from 1 to 9 collect
+  (loop for j from 1 to 9 collect
+  (name j i))))
 
 (defun units ()
-    (apply #'concatenate 'list
-     (loop for m from 0 to 2
-         collect (loop for n from 0 to 2
-                     collect (apply #'concatenate 'list
-                                    (loop for i from 0 to 2
-                                        collect (loop for j from 0 to 2
-                                                    collect (name (+ i (* m 3) 1) (+ j (* n 3) 1)))))))))
+  (apply #'concatenate 'list
+  (loop for m from 0 to 2 collect
+  (loop for n from 0 to 2 collect
+  (apply #'concatenate 'list
+  (loop for i from 0 to 2 collect
+  (loop for j from 0 to 2 collect
+  (name (+ i (* m 3) 1) (+ j (* n 3) 1)))))))))
 
 (defun add-cell-constraints (name net cells)
-  (mapcar #'(lambda (cell1)
-              (let ((cells2 (remove cell1 cells :TEST #'(lambda (x y) (funcall (network-name-test net) (cell-name x) (cell-name y))))))
-                (add-constraint-cell
-                 cell1
-                 (build-constraint (concatenate 'string name (cell-name cell1)) net
-                                   #'(lambda (constraint)
-                                       (when (known? cell1)
-                                         (loop for cell2 in cells2
-                                             when (member (value cell1) (cell-value cell2) :TEST (network-equality-test net))
-                                             do (queue-cell cell2 :EXCLUDE (value cell1) constraint))))))))
-          cells))
+  (mapcar
+   #'(lambda (cell1)
+       (let ((cells2 (remove cell1 cells :TEST #'(lambda (x y) (funcall (network-name-test net) (cell-name x) (cell-name y))))))
+         (add-constraint-cell
+          cell1
+          (build-constraint
+           (concatenate 'string name (cell-name cell1)) net
+           #'(lambda (constraint)
+               (when (known? cell1)
+                 (loop for cell2 in cells2
+                     when (member (value cell1) (cell-value cell2) :TEST (network-equality-test net)) do
+                       (queue-cell cell2 :EXCLUDE (value cell1) constraint))))))))
+   cells))
   
 (defun add-unit-constraints (name net css)
-  (loop for cs in css
-      do (add-cell-constraints name net (mapcar #'(lambda (c) (lookup-cell c net)) cs))))
+  (loop for cs in css do
+  (add-cell-constraints
+   name net
+   (loop for c in cs collect (lookup-cell c net)))))
 
 (defun add-all-constraints (net)
   (add-unit-constraints "row" net (rows))
@@ -85,10 +86,10 @@
 
 (defun solution (net)
   (apply #'vector
-         (loop for i from 1 to 9
-             collect (apply #'vector
-                            (loop for j from 1 to 9
-                                collect (value (lookup-cell (name i j) net)))))))
+  (loop for i from 1 to 9 collect
+  (apply #'vector
+  (loop for j from 1 to 9 collect
+  (value (lookup-cell (name i j) net)))))))
 
 (defun solve-sudoku (puzzle)
   (setq *net* (sudoku-solve puzzle))
