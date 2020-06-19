4e1f37e4851e655e1701eec948f3ba6f6f9dbdbb
Nada Amin <namin@seas.harvard.edu>
2020-06-19 17:47:03 -0400

    minor tweaks

--- BPS/relax/my.lisp
+++ BPS/relax/my.lisp
@@ -1,6 +1,7 @@
 (bps-load-file (make-bps-path "relax") "waltzer" :action :compile)
 
 (bps-load-file (make-bps-path "relax") "sudoku" :action :compile)
+(solve-sudoku *puzzle*)
 
 (bps-load-file (make-bps-path "relax") "scene" :action :compile)
 (analyze-scene *scene-file* *jcatalog-file*)
