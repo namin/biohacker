8c7d61d547cc32dc38b104f3a3b950683f27de3c
Nada Amin <namin@seas.harvard.edu>
2020-06-20 20:25:30 -0400

    examples of jsaint

--- BPS/jtms/my.lisp
+++ BPS/jtms/my.lisp
@@ -13,3 +13,11 @@
 
 (bps-load-file (make-bps-path "jtms") "sudoku" :action :compile)
 (solve-sudoku *easy-puzzle* :debugging t)
+
+(bps-load-file (make-bps-path "jtms") "match" :action :compile)
+(bps-load-file (make-bps-path "jtms") "simplify" :action :compile)
+(bps-load-file (make-bps-path "jtms") "jsaint" :action :compile)
+(try-jsaint problem2)
+(explain-result)
+(try-jsaint '(integrate (integral (+ (* 3 x) (cosh x)) x)))
+(explain-result)
