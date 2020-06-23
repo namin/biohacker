192aaccf0ff189657f922005e9dbdcf67d82c60f
Nada Amin <namin@seas.harvard.edu>
2020-06-22 21:37:07 -0400

    fix

--- BPS/ltms/my.lisp
+++ BPS/ltms/my.lisp
@@ -5,6 +5,7 @@
 (bps-load-file (make-bps-path "ltms") "cltms" :action :compile)
 
 (bps-load-file (make-bps-path "ltms") "ltms-ex" :action :compile)
+(test-explain)
 (run-tests)
 
 (bps-load-file (make-bps-path "ltms") "dds" :action :compile)
