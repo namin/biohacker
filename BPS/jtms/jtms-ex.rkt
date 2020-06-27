46d7bc6a429619dea89a2c31b1b881e0cfd4b76f
Nada Amin <namin@seas.harvard.edu>
2020-06-26 21:30:52 -0400

    example 3 working too

--- BPS/jtms/jtms-ex.rkt
+++ BPS/jtms/jtms-ex.rkt
@@ -28,12 +28,29 @@
   (enable-assumption nc)
   (enable-assumption nd)
 
-  ;;(explore-network ng)
-  ;;(explore-network nf)
+  (explore-network ng)
+  (explore-network nf)
 
   (define contra (tms-create-node *jtms* 'Loser #:contradictoryp #t))
   (justify-node 'j5 contra (list ne nf))
 
   )
 
+(define (ex3)
+  (define *jtms* (create-jtms "Multiple support example"))
+  (define assumption-a (tms-create-node *jtms* 'A #:assumptionp #t))
+  (define assumption-c (tms-create-node *jtms* 'C #:assumptionp #t))
+  (define assumption-e (tms-create-node *jtms* 'E #:assumptionp #t))
+  (define node-h (tms-create-node *jtms* 'h))
+  (enable-assumption assumption-a)
+  (enable-assumption assumption-c)
+  (enable-assumption assumption-e)
+  (justify-node 'R1 node-h (list assumption-c assumption-e))
+  (define node-g (tms-create-node *jtms* 'g))
+  (justify-node 'R2 node-g (list assumption-a assumption-c))
+  (define contradiction (tms-create-node *jtms*
+					 'CONTRADICTION #:contradictoryp #t))
+  (justify-node 'R3 contradiction (list node-g)))
+
 (ex1)
+(ex3)
