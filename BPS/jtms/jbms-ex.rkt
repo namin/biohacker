8f89744f41f8fd7b5617c02fb9b76390cf80602c
Nada Amin <namin@seas.harvard.edu>
2020-07-01 14:51:26 -0400

    cylic ex

--- BPS/jtms/jbms-ex.rkt
+++ BPS/jtms/jbms-ex.rkt
@@ -12,10 +12,11 @@
 
 (oplus (interval 0.5 1) (interval 0.5 1)) ;; div by zero but not following invariant
 
+(define ex1
 (define j (create-jbms "hello" #:debugging #t))
 (define na (tms-create-node j 'a #:belief (interval 0.1 0.6)))
 (define nb (tms-create-node j 'b #:belief (interval 0.2 0.5)))
-(define nf (tms-create-node j 'b #:belief (interval 0.3 0.4)))
+(define nf (tms-create-node j 'f #:belief (interval 0.3 0.4)))
 (justify-node 'j1 nf (list na nb))
 (why-node nf)
 (enable-assumption na) ;; still have to attacth the belief for now
@@ -25,3 +26,28 @@
 (tms-node-belief na)
 (tms-node-belief nb)
 (combine-beliefs nf na nb)
+)
+
+(define ex2
+  (define j (create-jbms "hello" #:debugging #t))
+  (define na (tms-create-node j 'a #:belief (interval 0.1 0.6)))
+  (define nb (tms-create-node j 'b #:belief (interval 0.2 0.5)))
+  (define nc (tms-create-node j 'c #:belief (interval 0.1 0.6)))
+  (define nd (tms-create-node j 'd #:belief (interval 0.2 0.5)))
+  (define ne (tms-create-node j 'e #:belief (interval 0.2 0.5)))
+  (justify-node 'j1 nb (list na))
+  (justify-node 'j2 nb (list nd))
+  (justify-node 'j2 nc (list nb))
+  (justify-node 'j2 nd (list nc))
+  (justify-node 'j2 nd (list ne))
+  (enable-assumption na) ;; still have to attacth the belief for now
+
+  (tms-node-belief na)
+  (tms-node-belief nb)
+  (tms-node-belief nc)
+  (tms-node-belief nd)
+
+  (enable-assumption ne)
+
+  (tms-node-belief nb) ;; B is not more supported than it should. See discussion on cycles.
+)
