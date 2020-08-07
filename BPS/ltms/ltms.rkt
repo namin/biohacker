c9e68f1f1d4e9889ec48b53c9b56cc52164a24c4
19katz <katherinezhang@college.harvard.edu>
2020-08-07 00:40:01 -0700

    merging changes

--- BPS/ltms/ltms.rkt
+++ BPS/ltms/ltms.rkt
@@ -113,12 +113,12 @@
 
 (define-syntax violated-clause?
   (syntax rules ()
-    [(_ clause ...)
+    [(_ clause)
      (= (clause-pvs clause) 0)]))
 
 (define-syntax walk-clauses
   (syntax rules ()
-     [(_ ltms f ...)
+     [(_ ltms f)
       (if (ltms-complete ltms)
           (walk-trie f (ltms-clauses ltms))
           (map f (ltms-clauses ltms)))]))
@@ -180,3 +180,265 @@
 (define  (true-node? node) (equal? (tms-node-label node) ':TRUE))
 (define  (false-node? node) (equal? (tms-node-label node) ':FALSE))
 
+;;; INSERT THE REST OF CODE HERE
+
+;;; Inquiring about well-founded support
+
+(define (support-for-node node)
+  (match-define (list result support) (list '() '()))
+  (cond ((empty? (tms-node-support node)) (set! support (tms-node-support node)))
+	((equal? support ':ENABLED-ASSUMPTION) ':ENABLED-ASSUMPTION)
+	(#t (for ((pair (clause-literals support)))
+	     (unless (equal? (car pair) node)
+	       (push (car pair) result)))
+	   (values result (clause-informant support)))))
+
+(define (assumptions-of-node node)
+  (cond ((equal? ':ENABLED-ASSUMPTION (tms-node-support node)) (list node))
+	((known-node? node)
+	 (assumptions-of-clause (tms-node-support node)))))
+
+(define (assumptions-of-clause in-clause) 
+  (do ((clause-queue (list in-clause)
+		     (append (cdr clause-queue) new-clauses))
+       (mark (list nil)) ;; Doesn't make sense even in ltms.lisp file should be (list nil). To be changes (maybe) after Kat's work
+       (node #f) 
+       (new-clauses '() '()) 
+       (assumptions '()))  
+      ((empty? clause-queue) assumptions)
+    (for ((term-pair (clause-literals (car clause-queue))))
+      (set! node (car term-pair))
+      (unless (equal? (tms-node-mark node) mark)
+	(unless (equal? (tms-node-label node) (cdr term-pair))
+	  (cond ((equal? ':ENABLED-ASSUMPTION (tms-node-support node))
+		 (push node assumptions))
+		((empty? (tms-node-support node)) (ltms-error "Node is unknown" node))
+		(#t (push (tms-node-support node) new-clauses))))
+	(set-tms-node-mark! node mark)))))
+
+;;; Simple user interface
+
+(define *contra-assumptions* '())
+
+(define (ask-user-handler contradictions ltms)
+  (for ((contradiction contradictions))
+    (when (violated-clause? contradiction)
+	(handle-one-contradiction contradiction))))
+
+(define (handle-one-contradiction violated-clause) ;; TODO Apoorv
+   (let ((*contra-assumptions* (assumptions-of-clause violated-clause))
+         (the-answer #f))
+      (when (empty? *contra-assumptions*) (ltms-error "Global contradiction"
+                                    violated-clause))
+      (format "~%Contradiction found:")
+      (print-contra-list *contra-assumptions*)
+      (format  "~%Call (TMS-ANSWER <number>) to retract assumption.")
+      (set! the-answer
+         (catch 'tms-contradiction-handler
+            (cerror "Continue LTRE processing (after retracting an assumption)"
+               "LTMS contradiction break")))
+      (when the-answer
+        (retract-assumption (list-ref *contra-assumptions* (- the-answer 1))))))
+
+
+(define (print-contra-list nodes)
+  (do ((counter 1 (+ 1 counter))
+       (nn nodes (cdr nn)))
+      ((empty? nn))
+    (format "~%~A ~A" counter
+	    (node-string (car nn)))))
+
+
+(define (tms-answer num) ;; TODO Apoorv 
+  (if (integer? num)
+      (if (> num 0)
+	  (if (not (> num (length *contra-assumptions*)))
+	      (throw 'tms-contradiction-handler num)
+	      (format  "~%Ignoring answer, too big."))
+	  (format  "~%Ignoring answer, too small"))
+      (format  "~%Ignoring answer, must be an integer.")))
+
+(define (avoid-all contradictions ignore)
+  (match-define (list culprits culprit sign) (list '() #f #f)) ;;
+  (for ((contradiction contradictions))
+    (when (violated-clause? contradiction)
+      (set! culprits (assumptions-of-clause contradiction))
+      (when (empty? culprits) 
+	(ltms-error "Total contradiction" contradiction))
+      (set! culprit (car culprits))
+      (set! sign (tms-node-label culprit))
+      (retract-assumption culprit)
+      (add-nogood culprit sign culprits) ;; to be defined above Kat's 
+      #t)))
+
+(define (clause-antecedents clause)
+  (let ((result '()))
+  (for ((pair (clause-literals clause)))
+    (unless (equal? (tms-node-support (car pair)) clause)
+      (push (car pair) result)))
+  result
+  ))
+
+(define (signed-node-string node)
+  (if (true-node? node) (node-string node)
+      (cond
+       ((false-node? node) (format "Not[~A]" (node-string node)))
+       (else (format "Unknown[~A]" (node-string node))))))
+
+(define (node-consequences node)
+  (match-define (list conseq conseqs) (list #f #f))
+  (for ((cl (ecase (tms-node-label node)
+		(':TRUE (tms-node-false-clauses node))
+		(':FALSE (tms-node-true-clauses node)))))
+    (unless (equal? cl (tms-node-support node))
+      (set! conseq (clause-consequent cl))
+      (when conseq (push conseq conseqs))))
+  conseqs)
+
+(define (why-node node)
+  (cond ((unknown-node? node)
+	 (format  "~A is unknown." (node-string node)))
+	((equal? ':ENABLED-ASSUMPTION (tms-node-support node))
+	 (format  "~A is ~A <~A>"
+		 (node-string node)
+		 (tms-node-label node) (tms-node-support node)))
+	(else (format "~A is ~A via ~A on"
+		   (node-string node)
+		   (tms-node-label node)
+		   (or (clause-informant (tms-node-support node))
+		       (tms-node-support node)))
+	   (for ((term-pair (clause-literals (tms-node-support node))))
+	     (unless (equal? (tms-node-label (car term-pair))
+			    (cdr term-pair))
+	       (format "~A is ~A"
+		       (node-string (car term-pair))
+		       (tms-node-label (car term-pair)))))))
+  node)
+
+(define (why-nodes ltms)
+  (hash-map  (ltms-nodes ltms) (lambda (ignore n) (why-node n))))
+
+(define *line-count* 0)
+
+(define (explain-node node)
+  (define *line-count* #f)
+  (unless (equal? (tms-node-label node) ':UNKNOWN)
+    (set! *line-count* 0)
+    (hash-map (lambda (ignore node) (set-tms-node-mark! node #f))
+	   (ltms-nodes (tms-node-ltms node)))
+    (explain-1 node)))
+
+
+(define (explain-1 node)
+  (define antecedents '())
+  (cond ((tms-node-mark node))
+	((equal ':ENABLED-ASSUMPTION (tms-node-support node))
+         (if (true-node? node)
+             (format "~A  ~A () Assumption"
+                     (incf *line-count*) (node-string node))
+             (format "~A (:NOT ~A)  () Assumption" (incf *line-count*) (node-string node)))
+	 (set-tms-node-mark! node *line-count*))
+	(else (set! antecedents
+		 (map explain-1 (clause-antecedents (tms-node-support node))))
+              (cond
+                ((true-node? node) (format "~A ~A ~A" (incf *line-count*) (node-string node) antecedents))
+                (else (format "~A (:NOT ~A) ~A" (incf *line-count*) (node-string node) antecedents)))
+
+	   (pretty-print-clause (tms-node-support node))
+	   (set-tms-node-mark node *line-count*))))
+
+(define (pretty-print-clauses ltms)
+  (walk-clauses ltms (lambda (l)
+                       (pretty-print-clause l))))
+
+(define (pretty-print-clause clause)
+  (format "(:OR")
+  (for ((literal (clause-literals clause)))
+    (if (equal? ':TRUE (cdr literal))
+        (format " ~A" (node-string (car literal)))
+	(format "(:NOT ~A)" (node-string (car literal)))))
+  (format  ")"))
+
+
+(define (show-node-consequences node)
+  (let ((conseqs (node-consequences node)))
+    (cond ((not (empty? conseqs)) 
+	   (printf (format  "~% Consequences of ~A:" (signed-node-string node)))
+	   (for ((conseq conseqs))
+		   (printf (format  "~%  ~A" (signed-node-string conseq)))))
+	  (else (printf (format "~% ~A has no consequences." (node-string node)))))))
+
+(define (node-show-clauses node)
+  (printf (format  "For ~A:" (node-string node)))
+  (for ((cl (tms-node-true-clauses node)))
+    (printf (format T "~%")) (pretty-print-clause cl))
+  (for ((cl (tms-node-false-clauses node)))
+    (printf (format T "~%")) (pretty-print-clause cl)))
+
+
+(define (explore-network node)
+  (with-handlers ((ret (lambda (x) x)))
+  (unless (known-node? node)
+	  (printf (format "~% Sorry, ~A not believed." (node-string node)))
+	  (raise node))
+  (do ((stack '())
+       (current node)
+       (mode ':ante)
+       (options '())
+       (olen 0)
+       (done? #f))
+      (done? current)
+      (cond ((equal? mode ':ante)
+	     (why-node current)
+	     (set! options (if (clause? (tms-node-support current))
+			       (clause-antecedents (tms-node-support current))
+			     '())))
+	    (else ;; Looking at consequences
+	     (show-node-consequences current)
+	     (set! options (node-consequences current))))
+      (set! olen (length options))
+      (do ((good? nil)
+	   (choice 0))
+	  (good? (case good?
+		       ((q) (raise current))
+		       ((c) (set! mode ':conseq))
+		       ((a) (setq mode ':ante))
+		       ((0) (unless (empty? stack)
+			      (set! current (pop stack))
+			      (raise current)))
+		       ((#t) (push current stack)
+			  (set! current (list-ref options (- good? 1))))))
+	  (printf "\n>>>")
+	  (set! choice (read))
+	  (if (or (equal? choice 'q)
+		  (equal? choice 'c)
+		  (equal? choice 'a)
+		  (and (integer? choice)
+		       (not (> choice olen))
+		       (not (< choice 0))))
+	      (set! good? choice)
+	      (printf (format "~% Must be q, a, c or an integer from 0 to ~D."
+		        olen)))))))
+
+;;;; Add-on utilities for easier lisp to racket translation;;;;;;;
+(define-syntax-rule (incf x)
+  (begin
+    (set! x (+ 1 x))
+  x)
+  )
+
+(define (ret x)
+  #t)
+(define (is-true X)
+(define-syntax-rule (push val lst)
+  (set! lst (cons val lst))))
+
+(define-syntax-rule (pop lst)
+  (begin
+    (define popped (car lst))
+  (set! lst (cdr lst))
+  popped
+  )
+  )
+(define-syntax-rule (pushnew val lst)
+  (unless (member? val lst) (set! lst (cons val lst))))
