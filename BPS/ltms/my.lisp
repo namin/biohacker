6ec3fa1fd72ad4b697f23da2923e799fb077a232
Nada Amin <namin@seas.harvard.edu>
2020-08-11 12:52:24 -0400

    add simple example

--- BPS/ltms/my.lisp
+++ BPS/ltms/my.lisp
@@ -8,6 +8,15 @@
 (test-explain)
 (run-tests)
 
+;; pages 292-293 of BPS
+(setq *ltms* (create-ltms "Simple"))
+(setq x (tms-create-node *ltms* "x" :ASSUMPTIONP t) y (tms-create-node *ltms* "y") z (tms-create-node *ltms* "z") r (tms-create-node *ltms* "r"))
+(add-formula *ltms* `(:OR ,x ,y))
+(add-formula *ltms* `(:OR (:NOT ,y) ,z))
+(add-formula *ltms* `(:OR (:NOT ,z) ,r))
+(enable-assumption x :FALSE)
+(explain-node r)
+
 (bps-load-file (make-bps-path "ltms") "dds" :action :compile)
 (Test-DD-search)
 
