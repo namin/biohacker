3d44d27cfcf324bdca96a0bf160a1b9c2a9b5012
Nada Amin <namin@seas.harvard.edu>
2020-09-14 00:23:24 -0400

    a bit more

--- BPS/jtms/jsaint.rkt
+++ BPS/jtms/jsaint.rkt
@@ -196,8 +196,16 @@
 
 ;;;; Auxiliary routines
 
-(define (fetch-solution problem)
-  'TODO)
+(define (fetch-solution problem [js *jsaint*])
+  (with-jsaint
+   js
+   (with-handlers
+    ([(lambda (x) #t)
+      (lambda (x) x)])
+    (let ((jtre (jsaint-jtre *jsaint*)))
+      (for ([solution (fetch `(solution of ,problem ?answer) jtre)])
+           (when (in? solution jtre)
+             (raise (third solution))))))))
 
 (define (jsaint-contradiction-handler contradictions jtms)
   (ask-user-handler contradictions jtms)) ;; default
