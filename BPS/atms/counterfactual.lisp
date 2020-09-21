f991d9eb83c0cde7c0941572679facffbb9c919e
Nada Amin <namin@seas.harvard.edu>
2020-09-21 13:40:53 -0400

    true counterfactual

--- BPS/atms/counterfactual.lisp
+++ BPS/atms/counterfactual.lisp
@@ -4,21 +4,30 @@
 (setq a (tms-create-node *atms* "A"))
 (setq b (tms-create-node *atms* "B"))
 (setq d (tms-create-node *atms* "D"))
+(setq c* (tms-create-node *atms* "C*"))
+(setq a* (tms-create-node *atms* "A*"))
+(setq b* (tms-create-node *atms* "B*"))
+(setq d* (tms-create-node *atms* "D*"))
 
 (setq nu (tms-create-node *atms* "notU"))
 (setq nc (tms-create-node *atms* "notC"))
 (setq na (tms-create-node *atms* "notA"))
 (setq nb (tms-create-node *atms* "notB"))
 (setq nd (tms-create-node *atms* "notD"))
+(setq nc* (tms-create-node *atms* "notC*"))
+(setq na* (tms-create-node *atms* "notA*"))
+(setq nb* (tms-create-node *atms* "notB*"))
+(setq nd* (tms-create-node *atms* "notD*"))
 
 (nogood-nodes 'nogood-u (list u nu))
 (nogood-nodes 'nogood-c (list c nc))
 (nogood-nodes 'nogood-a (list a na))
 (nogood-nodes 'nogood-b (list b nb))
 (nogood-nodes 'nogood-d (list d nd))
-
-(assume-node u)
-(assume-node nu)
+(nogood-nodes 'nogood-c (list c* nc*))
+(nogood-nodes 'nogood-a (list a* na*))
+(nogood-nodes 'nogood-b (list b* nb*))
+(nogood-nodes 'nogood-d (list d* nd*))
 
 (justify-node 'J_UC c (list u))
 (justify-node 'J_CU u (list c))
@@ -41,21 +50,29 @@
 (justify-node 'JN_DB nb (list nd))
 (justify-node 'JN_ABD nd (list na nb))
 
+(justify-node 'J_UC c* (list u))
+(justify-node 'J_CU u (list c*))
+;;(justify-node 'J_CA a* (list c*))
+;;(justify-node 'J_AC c* (list a*))
+(justify-node 'J_CB b* (list c*))
+(justify-node 'J_BC c* (list b*))
+(justify-node 'J_AD d* (list a*))
+(justify-node 'J_DA a* (list d* nb*))
+(justify-node 'J_BD d* (list b*))
+(justify-node 'J_DB b* (list d* na*))
 
-(why-nodes *atms*)
-#|
-<The contradiction,{}>
-<U,{{U}}>
-<C,{{U}}>
-<A,{{U}}>
-<B,{{U}}>
-<D,{{U}}>
-<notU,{{notU}}>
-<notC,{{notU}}>
-<notA,{{notU}}>
-<notB,{{notU}}>
-<notD,{{notU}}>
-|#
+(justify-node 'JN_UC nc* (list nu))
+(justify-node 'JN_CU nu (list nc*))
+;;(justify-node 'JN_CA na* (list nc*))
+;;(justify-node 'JN_AC nc* (list na*))
+(justify-node 'JN_CB nb* (list nc*))
+(justify-node 'JN_BC nc* (list nb*))
+(justify-node 'JN_DA na* (list nd*))
+(justify-node 'JN_DB nb* (list nd*))
+(justify-node 'JN_ABD nd* (list na* nb*))
+
+(assume-node u)
+(assume-node nu)
 
 (assume-node c)
 (assume-node nc)
@@ -66,17 +83,39 @@
 (assume-node d)
 (assume-node nd)
 
+(assume-node c*)
+(assume-node nc*)
+(assume-node a*)
+(assume-node na*)
+(assume-node b*)
+(assume-node nb*)
+(assume-node d*)
+(assume-node nd*)
+
 (why-nodes *atms*)
 #|
 <The contradiction,{}>
-<U,{{D}{B}{A}{C}{U}}>
-<C,{{D}{B}{A}{C}{U}}>
-<A,{{D}{B}{A}{C}{U}}>
-<B,{{D}{B}{A}{C}{U}}>
-<D,{{D}{B}{A}{C}{U}}>
-<notU,{{notD}{notB}{notA}{notC}{notU}}>
-<notC,{{notD}{notB}{notA}{notC}{notU}}>
-<notA,{{notD}{notB}{notA}{notC}{notU}}>
-<notB,{{notD}{notB}{notA}{notC}{notU}}>
-<notD,{{notD}{notB}{notA}{notC}{notU}}>
+<U,{{D*,notA*}{B*}{C*}{D}{B}{A}{C}{U}}>
+<C,{{D*,notA*}{B*}{C*}{D}{B}{A}{C}{U}}>
+<A,{{D*,notA*}{B*}{C*}{D}{B}{A}{C}{U}}>
+<B,{{D*,notA*}{B*}{C*}{D}{B}{A}{C}{U}}>
+<D,{{D*,notA*}{B*}{C*}{D}{B}{A}{C}{U}}>
+<C*,{{D*,notA*}{B*}{C*}{D}{B}{A}{C}{U}}>
+<A*,{{D*,notB*}{D*,notC*}{D*,notD}{D*,notB}{D*,notA}{D*,notC}{D*,notU}{A*}}>
+<B*,{{D*,notA*}{B*}{C*}{D}{B}{A}{C}{U}}>
+<D*,{{D*}{B*}{A*}{C*}{D}{B}{A}{C}{U}}>
+<notU,{{notD*}{notB*}{notC*}{notD}{notB}{notA}{notC}{notU}}>
+<notC,{{notD*}{notB*}{notC*}{notD}{notB}{notA}{notC}{notU}}>
+<notA,{{notD*}{notB*}{notC*}{notD}{notB}{notA}{notC}{notU}}>
+<notB,{{notD*}{notB*}{notC*}{notD}{notB}{notA}{notC}{notU}}>
+<notD,{{notD*}{notB*}{notC*}{notD}{notB}{notA}{notC}{notU}}>
+<notC*,{{notD*}{notB*}{notC*}{notD}{notB}{notA}{notC}{notU}}>
+<notA*,{{notD*}{notA*}}>
+<notB*,{{notD*}{notB*}{notC*}{notD}{notB}{notA}{notC}{notU}}>
+<notD*,{{notD*}{notA*,notB*}{notA*,notC*}{notA*,notD}{notA*,notB}{notA,notA*}{notA*,notC}{notA*,notU}}>
 |#
+
+;; prediction: notA implies notD
+;; abduction: notD implies notC
+;; transduction: A implies B
+;; action: notC and A* implies D* and notB*
