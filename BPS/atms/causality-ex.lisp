2d4adb4bfefa9d28944266c93081ccecc409c092
Nada Amin <namin@seas.harvard.edu>
2020-11-11 12:16:33 -0500

    move extra causality examples to its own ex file

---
+++ BPS/atms/causality-ex.lisp
@@ -0,0 +1,105 @@
+;;  Query we would like is :given '(Xray Dys)
+;;    :intervention '((:NOT Cancer) (:NOT TB))  ;; expected sufficiency of Bronchitis
+
+;;  Given an observed EHR, how many positive symptoms are expected to go away due to treating the disease?
+(setq *expected-disablement*
+      (make-causal
+       :title "How would the symptoms change be if we knew that Bronchitis was not the disease"
+       :graph
+       '((TB . Dys)
+         (TB . Xray)
+	 (Cancer . Dys)
+	 (Cancer . Xray)
+	 (Bronchitis . Dys))
+       :priors
+       '((TB . 0.02)
+	 (Cancer . 0.5)
+	 (Bronchitis . 0.3))
+       :symbolic-priors
+       '((TB . t)
+	 (Cancer . c)
+	 (Bronchitis . b))
+       :given '(:AND Dys (:NOT Xray))  ;; Observed Symptoms  (S)  S+ = {Dys}
+       :intervention '(:NOT Bronchitis)  ;; Do(Bronchitis = F)
+       :outcome '(:AND Dys (:NOT Xray))))  ;; Counterfactual Symptoms (S')  S'+ = {Dys'  Nausea'}
+;; expectation:  If Bronchitis is the disease then {Dys', Xray'}|{Dys = T, Dys' = F}|P( {Dys'=T, Xray'=F}  | do(Bronchitis = F), {Dys=T, Xray=F})
+;;                                               < P( {Dys'=T, Xray'= F} | do(Bronchitis = T), {Dys=T, Xray=F})
+
+
+
+
+;;  given an observed EHR, how many symptoms would remain after treating every disease but one?
+
+
+(setq *expected-sufficiency*
+      (make-causal
+       :title "How would the symptoms change if we treated the patient for Cancer and TB?"
+       :graph
+       '((TB . Dys)
+         (TB . Xray)
+	 (Cancer . Dys)
+	 (Cancer . Xray)
+	 (Bronchitis . Dys))
+       :priors
+       '((TB . 0.02)
+	 (Cancer . 0.5)
+	 (Bronchitis . 0.3))
+       :symbolic-priors
+       '((TB . t)
+	 (Cancer . c)
+	 (Bronchitis . b))
+       :given '(:AND Dys (:NOT Xray))   ;; S
+       :intervention '(:AND (:NOT TB) (:NOT Cancer))   ;; do(treat TB and Cancer)
+       :outcome '(:AND Dys (:NOT Xray))))  ;; S'
+
+
+;;
+
+
+
+(setq *necessary-causation*
+      (make-causal
+       :title "P({No Dys}_{treated Bronchitis} | Bronchitis, Dys)"
+       :graph
+       '((TB . Dys)
+         (TB . Xray)
+	 (Cancer . Dys)
+	 (Cancer . Xray)
+	 (Bronchitis . Dys))
+       :priors
+       '((TB . 0.02)
+	 (Cancer . 0.5)
+	 (Bronchitis . 0.3))
+       :symbolic-priors
+       '((TB . t)
+	 (Cancer . c)
+	 (Bronchitis . b))
+       :given '(:AND Dys Bronchitis)
+       :intervention '(:NOT Bronchitis)
+       :outcome '(:NOT Dys)))
+
+;; uh oh, getting negative probabilities
+;;(causal-crank *necessary-causation*)
+
+ (setq *sufficient-causation*
+      (make-causal
+       :title "P({Dys}_{infect with Bronchitis} | no_Bronchitis, no_Dys) %% P(Bronchtis) = .30 %% P(Dys | Bronchitis) = ? %% P(Bronchitis | Dys)"
+       :graph
+       '((TB . Dys)
+         (TB . Xray)
+	 (Cancer . Dys)
+	 (Cancer . Xray)
+	 (Bronchitis . Dys))
+       :priors
+       '((TB . 0.02)
+	 (Cancer . 0.5)
+	 (Bronchitis . 0.3))
+       :symbolic-priors
+       '((TB . t)
+	 (Cancer . c)
+	 (Bronchitis . b))
+       :given '(:AND (:NOT Dys) (:NOT Bronchitis))
+       :intervention 'Bronchitis
+       :outcome 'Dys))
+
+;;(causal-crank *sufficient-causation*)
