6cca184e84e0a0d851728e10079cc825e3664577
Nada Amin <namin@seas.harvard.edu>
2020-11-12 23:48:29 -0500

    remove unused code

--- BPS/atms/prob.lisp
+++ BPS/atms/prob.lisp
@@ -52,38 +52,3 @@
   :* #'symbolic-*
   :- #'symbolic--
   :/ #'symbolic-/))
-
-(defun numeric-env-prob (n e ps)
-    (let* ((as (env-assumptions e))
-           (kps (mapcar #'(lambda (k) (assoc k ps)) as)))
-      (if (some #'(lambda (kp) (not kp)) kps)
-          nil
-          (apply (numeric-* n) (mapcar #'cdr kps)))))
-
-(defun numeric-label-prob (n l ps)
-  (numeric-union-prob n (remove nil (mapcar #'(lambda (e) (numeric-env-prob n e ps)) l))))
-
-(defun choose (vs k)
-  (cond
-    ((= k 0) '(()))
-    ((null vs) '())
-    (t
-     (append
-      (mapcar #'(lambda (x) (cons (car vs) x)) (choose (cdr vs) (- k 1)))
-      (choose (cdr vs) k)))))
-
-;; P(A U B) = P(A) + P(B) - P(A)*P(B)
-;; P(A U B C) = P(A) + P(B) + P(C) - P(A)*P(B) - P(A)*P(C) - P(B)*P(C) + P(A)*P(B)*P(C)
-(defun numeric-union-prob (n vs)
-  (numeric-union-prob-iter n vs 1 (length vs)))
-
-(defun numeric-union-prob-iter (n vs k x)
-  (let ((r (choose vs k)))
-    (if (null r)
-        0
-        (funcall (numeric-- n)
-         (apply (numeric-+ n) (mapcar #'(lambda (x) (apply (numeric-* n) x)) r))
-         (numeric-union-prob-iter n vs (+ k 1) x)))))
-
-(defun numeric-node-prob (n x ps)
-  (numeric-label-prob n (tms-node-label x) ps))
