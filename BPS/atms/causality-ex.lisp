4cc6ec86466e4f9270485c82ffa6690f3a442e84
Nada Amin <namin@seas.harvard.edu>
2021-01-10 10:27:45 -0500

    dry

--- BPS/atms/causality-ex.lisp
+++ BPS/atms/causality-ex.lisp
@@ -93,24 +93,33 @@ Maybe we need a smarter simplifer. :P
 ;;  Query we would like is :given '(Xray Dys)
 ;;    :intervention '((:NOT Cancer) (:NOT TB))  ;; expected sufficiency of Bronchitis
 
+(setq
+ *medical-graph*
+ '((TB . Dys)
+   (TB . Xray)
+   (Cancer . Dys)
+   (Cancer . Xray)
+   (Bronchitis . Dys)))
+
+(setq
+ *medical-priors*
+ '((TB . 0.02)
+   (Cancer . 0.5)
+   (Bronchitis . 0.3)))
+
+(setq
+ *medical-symbolic-priors*
+ '((TB . t)
+   (Cancer . c)
+   (Bronchitis . b)))
+
 ;;  Given an observed EHR, how many positive symptoms are expected to go away due to treating the disease?
 (setq *expected-disablement*
       (make-causal
        :title "How would the symptoms change be if we knew that Bronchitis was not the disease"
-       :graph
-       '((TB . Dys)
-         (TB . Xray)
-	 (Cancer . Dys)
-	 (Cancer . Xray)
-	 (Bronchitis . Dys))
-       :priors
-       '((TB . 0.02)
-	 (Cancer . 0.5)
-	 (Bronchitis . 0.3))
-       :symbolic-priors
-       '((TB . t)
-	 (Cancer . c)
-	 (Bronchitis . b))
+       :graph *medical-graph*
+       :priors *medical-priors*
+       :symbolic-priors *medical-symbolic-priors*
        :given '(:AND Dys (:NOT Xray))  ;; Observed Symptoms  (S)  S+ = {Dys}
        :intervention '(:NOT Bronchitis)  ;; Do(Bronchitis = F)
        :outcome '(:AND Dys (:NOT Xray))))  ;; Counterfactual Symptoms (S')  S'+ = {Dys'  Nausea'}
@@ -135,20 +144,9 @@ Outcome probability: 0.00.
 (setq *expected-sufficiency*
       (make-causal
        :title "How would the symptoms change if we treated the patient for Cancer and TB?"
-       :graph
-       '((TB . Dys)
-         (TB . Xray)
-	 (Cancer . Dys)
-	 (Cancer . Xray)
-	 (Bronchitis . Dys))
-       :priors
-       '((TB . 0.02)
-	 (Cancer . 0.5)
-	 (Bronchitis . 0.3))
-       :symbolic-priors
-       '((TB . t)
-	 (Cancer . c)
-	 (Bronchitis . b))
+       :graph *medical-graph*
+       :priors *medical-priors*
+       :symbolic-priors *medical-symbolic-priors*
        :given '(:AND Dys (:NOT Xray))   ;; S
        :intervention '(:AND (:NOT TB) (:NOT Cancer))   ;; do(treat TB and Cancer)
        :outcome '(:AND Dys (:NOT Xray))))  ;; S'
@@ -168,20 +166,9 @@ Outcome probability: 1.00
 (setq *necessary-causation*
       (make-causal
        :title "P({No Dys}_{treated Bronchitis} | Bronchitis, Dys)"
-       :graph
-       '((TB . Dys)
-         (TB . Xray)
-	 (Cancer . Dys)
-	 (Cancer . Xray)
-	 (Bronchitis . Dys))
-       :priors
-       '((TB . 0.02)
-	 (Cancer . 0.5)
-	 (Bronchitis . 0.3))
-       :symbolic-priors
-       '((TB . t)
-	 (Cancer . c)
-	 (Bronchitis . b))
+       :graph *medical-graph*
+       :priors *medical-priors*
+       :symbolic-priors *medical-symbolic-priors*
        :given '(:AND Dys Bronchitis)
        :intervention '(:NOT Bronchitis)
        :outcome '(:NOT Dys)))
@@ -201,20 +188,9 @@ Outcome probability: (/ (* (- 1 T) (- 1 C) B) (+ (* T C B) (* T (- 1 C) B) (* (-
  (setq *sufficient-causation*
       (make-causal
        :title "P({Dys}_{infect with Bronchitis} | no_Bronchitis, no_Dys) %% P(Bronchtis) = .30 %% P(Dys | Bronchitis) = ? %% P(Bronchitis | Dys)"
-       :graph
-       '((TB . Dys)
-         (TB . Xray)
-	 (Cancer . Dys)
-	 (Cancer . Xray)
-	 (Bronchitis . Dys))
-       :priors
-       '((TB . 0.02)
-	 (Cancer . 0.5)
-	 (Bronchitis . 0.3))
-       :symbolic-priors
-       '((TB . t)
-	 (Cancer . c)
-	 (Bronchitis . b))
+       :graph *medical-graph*
+       :priors *medical-priors*
+       :symbolic-priors *medical-symbolic-priors*
        :given '(:AND (:NOT Dys) (:NOT Bronchitis))
        :intervention 'Bronchitis
        :outcome 'Dys))
