896bdba210af4a2da3b31eeeb41bdd57effbb372
Jeremy Zucker <djinnome@gmail.com>
2021-02-18 18:24:49 -0800

    Added counterfactual LTMS. Need to remove all code that does not contribute to the paper

--- BPS/ltms/counterfactual.lisp
+++ BPS/ltms/counterfactual.lisp
@@ -72,6 +72,76 @@
 (enable-assumption A-or-B=>D :TRUE)
 
 
+
+
+
+(setq *ltms* (create-ltms
+                "Rifleman Example"
+                :complete t))
+(setq
+ U (tms-create-node *ltms*
+     "Court orders" :ASSUMPTIONP t)
+ C (tms-create-node *ltms*
+    "Captain signals" :ASSUMPTIONP t)
+ A (tms-create-node *ltms*
+    "Rifleman A shoots" :ASSUMPTIONP t)
+ B (tms-create-node *ltms*
+     "Rifleman B shoots" :ASSUMPTIONP t)
+ D (tms-create-node *ltms*
+     "Prisoner dies" :ASSUMPTIONP t)
+ )
+
+(setq
+  U=>C (tms-create-node *ltms*
+      "Court orders => Captain signals" :ASSUMPTIONP t)
+  C=>A (tms-create-node *ltms*
+      "Captain signals => Rifleman A shoots" :ASSUMPTIONP t)
+  C=>B (tms-create-node *ltms*
+      "Captain signals => Rifleman B shoots" :ASSUMPTIONP t)
+  A-or-B=>D (tms-create-node *ltms*
+      "Rifleman A or B shoots => Prisoner dies" :ASSUMPTIONP t))
+
+(compile-formula *ltms*
+`(:IMPLIES "Court orders => Captain signals"
+  (:AND (:IMPLIES
+           "Court orders"
+           "Captain signals")
+        (:IMPLIES
+           (:NOT "Court orders")
+           (:NOT "Captain signals")))))
+(compile-formula *ltms*
+`(:IMPLIES "Captain signals => Rifleman A shoots"
+  (:AND (:IMPLIES
+            "Captain signals"
+            "Rifleman A shoots")
+        (:IMPLIES
+           (:NOT "Captain signals")
+           (:NOT "Rifleman A shoots")))))
+(compile-formula *ltms*
+`(:IMPLIES "Captain signals => Rifleman B shoots"
+  (:AND (:IMPLIES
+           "Captain signals"
+           "Rifleman B shoots")
+        (:IMPLIES
+           (:NOT "Captain signals")
+           (:NOT "Rifleman B shoots")))))
+(compile-formula *ltms*
+`(:IMPLIES "Rifleman A or B shoots => Prisoner dies"
+ (:AND (:IMPLIES
+          (:OR "Rifleman A shoots"
+               "Rifleman B shoots")
+          "Prisoner dies")
+       (:IMPLIES
+	(:AND
+	    (:NOT "Rifleman A shoots")
+            (:NOT "Rifleman B shoots"))
+	(:NOT "Prisoner dies")))))
+(enable-assumption U=>C :TRUE)
+(enable-assumption C=>A :TRUE)
+(enable-assumption C=>B :TRUE)
+(enable-assumption A-or-B=>D :TRUE)
+
+
 ;; \subsection{Prediction}
 ;; If rifleman A did not shoot,the prisoner is alive.
 ;; $$\lnot A\implies \lnot D$$
@@ -79,6 +149,64 @@
 
 (enable-assumption A :FALSE)
   (explain-node D)
+
+(retract-assumption A)
+
+
+(setq *ltms* (create-ltms "Rifleman Example" :complete t))
+(setq
+ U (tms-create-node *ltms* "u" :ASSUMPTIONP t)
+ U=>C (tms-create-node *ltms* "DepC" :ASSUMPTIONP t)
+ C (tms-create-node *ltms* "c" :ASSUMPTIONP t)
+ depA (tms-create-node *ltms* "DepA" :ASSUMPTIONP t)
+ A (tms-create-node *ltms* "a" :ASSUMPTIONP t)
+ depB (tms-create-node *ltms* "DepB" :ASSUMPTIONP t)
+ B (tms-create-node *ltms* "b" :ASSUMPTIONP t)
+ depD (tms-create-node *ltms* "DepD" :ASSUMPTIONP t)
+ D (tms-create-node *ltms* "d" :ASSUMPTIONP t)
+ U=>C* (tms-create-node *ltms* "DepC*" :ASSUMPTIONP t)
+ C* (tms-create-node *ltms* "c*" :ASSUMPTIONP t)
+ depA* (tms-create-node *ltms* "DepA*" :ASSUMPTIONP t)
+ A* (tms-create-node *ltms* "a*" :ASSUMPTIONP t)
+ depB* (tms-create-node *ltms* "DepB*" :ASSUMPTIONP t)
+ B* (tms-create-node *ltms* "b*" :ASSUMPTIONP t)
+ depD* (tms-create-node *ltms* "DepD*" :ASSUMPTIONP t)
+ D* (tms-create-node *ltms* "d*" :ASSUMPTIONP t)
+ )
+(compile-formula *ltms* `(:IMPLIES "DepC" (:IMPLIES "u" "c")))
+(compile-formula *ltms* `(:IMPLIES "DepC" (:IMPLIES "c" "u")))
+(compile-formula *ltms* `(:IMPLIES "DepA" (:IMPLIES "c" "a")))
+(compile-formula *ltms* `(:IMPLIES "DepA" (:IMPLIES "a" "c")))
+(compile-formula *ltms* `(:IMPLIES "DepB" (:IMPLIES "c" "b")))
+(compile-formula *ltms* `(:IMPLIES "DepB" (:IMPLIES "b" "c")))
+(compile-formula *ltms* `(:IMPLIES "DepD" (:IMPLIES "d" (:OR "a" "b"))))
+(compile-formula *ltms* `(:IMPLIES "DepD" (:IMPLIES (:OR "a" "b") "d")))
+
+(compile-formula *ltms* `(:IMPLIES "DepC*" (:IMPLIES "u" "c*")))
+(compile-formula *ltms* `(:IMPLIES "DepC*" (:IMPLIES "c*" "u")))
+(compile-formula *ltms* `(:IMPLIES "DepA*" (:IMPLIES "c*" "a*")))
+(compile-formula *ltms* `(:IMPLIES "DepA*" (:IMPLIES "a*" "c*")))
+(compile-formula *ltms* `(:IMPLIES "DepB*" (:IMPLIES "c*" "b*")))
+(compile-formula *ltms* `(:IMPLIES "DepB*" (:IMPLIES "b*" "c*")))
+(compile-formula *ltms* `(:IMPLIES "DepD*" (:IMPLIES "d*" (:OR "a*" "b*"))))
+(compile-formula *ltms* `(:IMPLIES "DepD*" (:IMPLIES (:OR "a*" "b*") "d*")))
+
+(enable-assumption depC :TRUE)
+(enable-assumption depA :TRUE)
+(enable-assumption depB :TRUE)
+(enable-assumption depD :TRUE)
+
+(enable-assumption depC* :TRUE)
+(enable-assumption depA* :TRUE)
+(enable-assumption depB* :TRUE)
+(enable-assumption depD* :TRUE)
+
+;; Counterfactual.
+;; Had the rifleman A not shooted, would it matter?
+(retract-assumption depA*)
+(enable-assumption a* :FALSE)
+(enable-assumption d :TRUE)
+(explain-node d*)
 
 
 
@@ -145,23 +273,23 @@
 
 (setq *ltms* (create-ltms "Rifleman Example" :complete t))
 (setq
- u (tms-create-node *ltms* "u" :ASSUMPTIONP t)
+ U (tms-create-node *ltms* "Court orders" :ASSUMPTIONP t)
  depC (tms-create-node *ltms* "DepC" :ASSUMPTIONP t)
- c (tms-create-node *ltms* "c" :ASSUMPTIONP t)
+ C (tms-create-node *ltms* "Captain signals" :ASSUMPTIONP t)
  depA (tms-create-node *ltms* "DepA" :ASSUMPTIONP t)
- a (tms-create-node *ltms* "a" :ASSUMPTIONP t)
+ A (tms-create-node *ltms* "Rifleman A shoots" :ASSUMPTIONP t)
  depB (tms-create-node *ltms* "DepB" :ASSUMPTIONP t)
- b (tms-create-node *ltms* "b" :ASSUMPTIONP t)
+ B (tms-create-node *ltms* "b" :ASSUMPTIONP t)
  depD (tms-create-node *ltms* "DepD" :ASSUMPTIONP t)
- d (tms-create-node *ltms* "d" :ASSUMPTIONP t)
+ D (tms-create-node *ltms* "d" :ASSUMPTIONP t)
  depC* (tms-create-node *ltms* "DepC*" :ASSUMPTIONP t)
- c* (tms-create-node *ltms* "c*" :ASSUMPTIONP t)
+ C* (tms-create-node *ltms* "c*" :ASSUMPTIONP t)
  depA* (tms-create-node *ltms* "DepA*" :ASSUMPTIONP t)
- a* (tms-create-node *ltms* "a*" :ASSUMPTIONP t)
+ A* (tms-create-node *ltms* "a*" :ASSUMPTIONP t)
  depB* (tms-create-node *ltms* "DepB*" :ASSUMPTIONP t)
- b* (tms-create-node *ltms* "b*" :ASSUMPTIONP t)
+ B* (tms-create-node *ltms* "b*" :ASSUMPTIONP t)
  depD* (tms-create-node *ltms* "DepD*" :ASSUMPTIONP t)
- d* (tms-create-node *ltms* "d*" :ASSUMPTIONP t)
+ D* (tms-create-node *ltms* "d*" :ASSUMPTIONP t)
  )
 (compile-formula *ltms* `(:IMPLIES "DepC" (:IMPLIES "u" "c")))
 (compile-formula *ltms* `(:IMPLIES "DepC" (:IMPLIES "c" "u")))
@@ -197,3 +325,128 @@
 (enable-assumption a* :FALSE)
 (enable-assumption d :TRUE)
 (explain-node d*)
+
+
+(setq *ltms* (create-ltms
+                "Rifleman Example"
+                :complete t))
+(setq
+ U (tms-create-node *ltms*
+     "Court orders" :ASSUMPTIONP t)
+ C (tms-create-node *ltms*
+    "Captain signals" :ASSUMPTIONP t)
+ A (tms-create-node *ltms*
+    "Rifleman A shoots" :ASSUMPTIONP t)
+ B (tms-create-node *ltms*
+     "Rifleman B shoots" :ASSUMPTIONP t)
+ D (tms-create-node *ltms*
+    "Prisoner dies" :ASSUMPTIONP t)
+ C* (tms-create-node *ltms*
+    "Captain* signals" :ASSUMPTIONP t)
+ A* (tms-create-node *ltms*
+    "Rifleman A* shoots" :ASSUMPTIONP t)
+ B* (tms-create-node *ltms*
+     "Rifleman B* shoots" :ASSUMPTIONP t)
+ D* (tms-create-node *ltms*
+     "Prisoner* dies" :ASSUMPTIONP t)
+ )
+
+(setq
+  U=>C (tms-create-node *ltms*
+      "Court orders => Captain signals" :ASSUMPTIONP t)
+  C=>A (tms-create-node *ltms*
+      "Captain signals => Rifleman A shoots" :ASSUMPTIONP t)
+  C=>B (tms-create-node *ltms*
+      "Captain signals => Rifleman B shoots" :ASSUMPTIONP t)
+  A-or-B=>D (tms-create-node *ltms*
+			     "Rifleman A or B shoots => Prisoner dies" :ASSUMPTIONP t)
+  U=>C* (tms-create-node *ltms*
+      "Court orders => Captain* signals" :ASSUMPTIONP t)
+  C*=>A* (tms-create-node *ltms*
+      "Captain signals => Rifleman A* shoots" :ASSUMPTIONP t)
+  C*=>B* (tms-create-node *ltms*
+      "Captain signals => Rifleman B* shoots" :ASSUMPTIONP t)
+  A*-or-B*=>D* (tms-create-node *ltms*
+      "Rifleman A* or B* shoots => Prisoner* dies" :ASSUMPTIONP t)
+
+  )
+
+(compile-formula *ltms*
+`(:IMPLIES "Court orders => Captain signals"
+  (:AND (:IMPLIES
+           "Court orders"
+           "Captain signals")
+        (:IMPLIES
+           (:NOT "Court orders")
+           (:NOT "Captain signals")))))
+(compile-formula *ltms*
+`(:IMPLIES "Captain signals => Rifleman A shoots"
+  (:AND (:IMPLIES
+            "Captain signals"
+            "Rifleman A shoots")
+        (:IMPLIES
+           (:NOT "Captain signals")
+           (:NOT "Rifleman A shoots")))))
+(compile-formula *ltms*
+`(:IMPLIES "Captain signals => Rifleman B shoots"
+  (:AND (:IMPLIES
+           "Captain signals"
+           "Rifleman B shoots")
+        (:IMPLIES
+           (:NOT "Captain signals")
+           (:NOT "Rifleman B shoots")))))
+(compile-formula *ltms*
+`(:IMPLIES "Rifleman A or B shoots => Prisoner dies"
+ (:AND (:IMPLIES
+          (:OR "Rifleman A shoots"
+               "Rifleman B shoots")
+          "Prisoner dies")
+       (:IMPLIES
+	(:AND
+	    (:NOT "Rifleman A shoots")
+            (:NOT "Rifleman B shoots"))
+	(:NOT "Prisoner dies")))))
+(compile-formula *ltms*
+`(:IMPLIES "Court orders => Captain* signals"
+  (:AND (:IMPLIES
+           "Court orders"
+           "Captain* signals")
+        (:IMPLIES
+           (:NOT "Court orders")
+           (:NOT "Captain* signals")))))
+(compile-formula *ltms*
+`(:IMPLIES "Captain* signals => Rifleman A* shoots"
+  (:AND (:IMPLIES
+            "Captain* signals"
+            "Rifleman A* shoots")
+        (:IMPLIES
+           (:NOT "Captain* signals")
+           (:NOT "Rifleman A* shoots")))))
+(compile-formula *ltms*
+`(:IMPLIES "Captain* signals => Rifleman B* shoots"
+  (:AND (:IMPLIES
+           "Captain* signals"
+           "Rifleman B* shoots")
+        (:IMPLIES
+           (:NOT "Captain* signals")
+           (:NOT "Rifleman B* shoots")))))
+(compile-formula *ltms*
+`(:IMPLIES "Rifleman A* or B* shoots => Prisoner* dies"
+ (:AND (:IMPLIES
+          (:OR "Rifleman A* shoots"
+               "Rifleman B* shoots")
+          "Prisoner* dies")
+       (:IMPLIES
+	(:AND
+	    (:NOT "Rifleman A* shoots")
+            (:NOT "Rifleman B* shoots"))
+	(:NOT "Prisoner* dies")))))
+
+(enable-assumption U=>C :TRUE)
+(enable-assumption C=>A :TRUE)
+(enable-assumption C=>B :TRUE)
+(enable-assumption A-or-B=>D :TRUE)
+(enable-assumption U=>C* :TRUE)
+(enable-assumption C*=>A* :TRUE)
+(enable-assumption C*=>B* :TRUE)
+(enable-assumption A*-or-B*=>D* :TRUE)
