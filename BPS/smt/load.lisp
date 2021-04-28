54e25ef9995426e62e698fb9839b19b718591165
Nada Amin <namin@seas.harvard.edu>
2021-04-28 15:51:55 -0400

    no need for special my branch as pull request got merged

--- BPS/smt/load.lisp
+++ BPS/smt/load.lisp
@@ -3,9 +3,7 @@
 ;; clone the following repositories
 ;; https://github.com/melisgl/named-readtables.git
 ;; https://github.com/trivial-gray-streams/trivial-gray-streams.git
-;; and instead of https://github.com/GrammaTech/cl-smt-lib clone
-;; https://github.com/namin/cl-smt-lib.git
-;; and checkout the my branch
+;; https://github.com/GrammaTech/cl-smt-lib.git
 (asdf:make 'named-readtables)
 (asdf:make 'trivial-gray-streams)
 (asdf:make 'cl-smt-lib)
