22a023a251b18cb1e8cb61044c10a985d3dd9c47
Nada Amin <namin@seas.harvard.edu>
2024-01-14 19:17:18 +0100

    more complex proof

--- BPS/ltms/mystery.lisp
+++ BPS/ltms/mystery.lisp
@@ -42,8 +42,8 @@
                             "Bob is guilty"))
 
 (compile-formula *ltms*
-                 `(:OR (:AND "Alice guilty" (:NOT "Bob guilty"))
-                       (:AND "Bob guilty" (:NOT "Alice guilty"))))
+                 `(:OR (:AND "Alice is guilty" (:NOT "Bob is guilty"))
+                       (:AND "Bob is guilty" (:NOT "Alice is guilty"))))
 
 (enable-assumption (find-node *ltms* "Alice has motive") :TRUE)
 (enable-assumption (find-node *ltms* "Alice has means") :TRUE)
@@ -61,4 +61,14 @@
   4 Alice is guilty         (1 2 3)  (:OR Alice is guilty (:NOT Alice has motive) (:NOT Alice has means) (:NOT Alice has opportunity))
 |#
 
-
+(retract-assumption (find-node *ltms* "Bob has motive"))
+(retract-assumption (find-node *ltms* "Alice has motive"))
+(enable-assumption (find-node *ltms* "Bob has motive") :TRUE)
+(explain-node (find-node *ltms* "Alice is guilty"))
+#|
+  1 Bob has opportunity             ()   Assumption
+  2   Bob has means             ()   Assumption
+  3  Bob has motive             ()   Assumption
+  4   Bob is guilty         (1 2 3)  (:OR Bob is guilty (:NOT Bob has motive) (:NOT Bob has means) (:NOT Bob has opportunity))
+  5 (:NOT Alice is guilty)             (4)  (:OR (:NOT Alice is guilty) (:NOT Bob is guilty))
+|#
